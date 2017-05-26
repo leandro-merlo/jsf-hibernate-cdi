86d818d8ed2e3056956c0784604105a3f2069a98
Leandro <leandro.merlo@gmail.com>
2017-05-26 01:41:49 -0300

    Adição de autenticação através da biblioteca Apache Shiro

--- src/main/java/br/com/leandro/bean/LoginBean.java
+++ src/main/java/br/com/leandro/bean/LoginBean.java
@@ -3,14 +3,22 @@ package br.com.leandro.bean;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+
 @Named
-@RequestScoped
+@ViewScoped
 public class LoginBean implements Serializable {
 
 	/**
@@ -23,23 +31,42 @@ public class LoginBean implements Serializable {
 
 	private String nomeUsuario;
 	private String senha;
-
+	
 	public String login() {
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken(nomeUsuario, senha);
 		FacesContext ctx = FacesContext.getCurrentInstance();
-		if ("admin".equals(nomeUsuario) && "123".equals(senha)) {
+		
+		try { 
+			subject.login(token);	
 			this.usuario.setNome(nomeUsuario);
 			this.usuario.setDataLogin(new Date());
 			return "/ConsultaLancamentos?faces-redirect=true";
-		} else {
-			FacesMessage mensagem = new FacesMessage("Usuário/senha inválidos!");
+		} catch (UnknownAccountException ex) {
+			FacesMessage mensagem = new FacesMessage("A conta informada não existe!");
 			mensagem.setSeverity(FacesMessage.SEVERITY_ERROR);
 			ctx.addMessage(null, mensagem);
-		}
+        } catch (IncorrectCredentialsException ex) {
+			FacesMessage mensagem = new FacesMessage("Usuário/senha inválido!");
+			mensagem.setSeverity(FacesMessage.SEVERITY_ERROR);
+			ctx.addMessage(null, mensagem);
+        } catch (LockedAccountException ex) {
+			FacesMessage mensagem = new FacesMessage("A conta informada está congelada!");
+			mensagem.setSeverity(FacesMessage.SEVERITY_ERROR);
+			ctx.addMessage(null, mensagem);
+        } catch (AuthenticationException ex) {
+			FacesMessage mensagem = new FacesMessage("Houve algum problema com o login!\nFavor, tente mais tarde");
+			mensagem.setSeverity(FacesMessage.SEVERITY_ERROR);
+			ctx.addMessage(null, mensagem);        	
+        } finally {
+            token.clear();
+        }
 		return null;
 	}
 
 	public String logout() {
-		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+//		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		SecurityUtils.getSubject().logout();
 		return "/Login?faces-redirect=true";
 	}
 
